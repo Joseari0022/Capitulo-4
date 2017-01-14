bcb9ba05209411c2bcf1fcd4ce746bf1fb561411
Jose Ariel Paredes Gonzalez <jose_paredes2@ucne.edu.do>
2017-01-14 12:56:08 +0100

    Add project files.

---
+++ Elevar_Numero/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elevar_Numero
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Numero = int.Parse(textBox1.Text);
+            double Potencia = int.Parse(textBox2.Text);
+            double Resultado;
+            Resultado = Math.Pow(Numero,Potencia);
+            listBox1.Items.Add(Resultado);
+        }
+    }
+}
---
+++ Tabla_Multiplicar/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabla_Multiplicar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int Tabla = int.Parse(textBox1.Text);
+            int Resultado;
+            for(int i = 1; i <= 10; i++)
+            {
+                Resultado = Tabla * i;
+                listBox1.Items.Add(Tabla+ "x" + i + "=" + Resultado);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
